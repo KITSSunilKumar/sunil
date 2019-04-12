c5858b4b1c925df4dba65db41abc42708e5e9e57
VandanaPC <VandanaPC@Vandana>
2019-04-12 10:45:36 +0530

    Hi this is for commit

---
+++ maven-demo/src/main/java/pakage/App.java
@@ -0,0 +1,8 @@
+package pakage;
+
+public class App {
+public String sample() {
+	return "sample";
+}
+	
+} 
---
+++ maven-demo/src/test/java/pakage/NewTest.java
@@ -0,0 +1,10 @@
+package pakage;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("sunil");
+  }
+}
